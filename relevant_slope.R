81e3078084e1a4ed751dd8defff54b6663c8ddf4
bd77 <bartdegraeuwe@hotmail.com>
2018-11-03 00:03:26 +0100

    slope attributes added and plot of each point improved with a local zoom

--- relevant_slope.R
+++ relevant_slope.R
@@ -2,24 +2,57 @@
 # function to determine the RSA (relevant slope area)
 # ---------------------------------------------------
 
+slopeProps <- setClass("slopeProps",
+                       slots = c(borderLines = "SpatialLines",
+                                 relevantSlopePolygon = "SpatialPolygons",
+                                 maxSlope = "SpatialPointsDataFrame",
+                                 meanSlope = "numeric"))
 
 # auxiliary functions
-run.up <- function(start.point, r.aspect, step, max.dist) {
-  # determine the point step meters uphill from the input point
+run.away <- function(start.point, r.aspect, step, max.dist, step.dir) {
+  # input:
+  # start.point: point from which a step is made
+  # r.aspect: raster of aspect angle in radians clockwise from North
+  # step: size of the step in meters
+  # max.dist: maximum distance from start.point in meters
+  # output: a Line object of points running from the start.point towards the specified
+  # direction
+  
+  # define the correct function to convert the aspect in a direction vector
+  if (step.dir == "up") { 
+    aspect2vector <- function(aspect) { c(-sin(aspect), -cos(aspect)) }
+  } else if (step.dir == "down") { 
+    aspect2vector <- function(aspect) { c(sin(aspect), cos(aspect)) }
+  } else if (step.dir == "right") { 
+    aspect2vector <- function(aspect) { c(cos(aspect), -sin(aspect)) }  
+  } else if (step.dir == "left") { 
+    aspect2vector <- function(aspect) { c(-cos(aspect), sin(aspect)) }
+  } else { 
+    print("Invalid direction for run.away function")
+  }
+  
+  # determine the point step meters from the input point in the direction
   dist2start <- 0
-  uphill.line <- start.point
+  run.away.line <- start.point
   current.point <- start.point
+  # limit the number of steps because the line can get stuck on a ridge
+  # or sink closer then max.dist
   n.iter <- 0
   max.iter <- 2*max.dist/step
+  # create a line of points running in the desired direction
   while (dist2start <= max.dist & n.iter < max.iter) {
+    # retrieve the aspect of the current position
     aspect.cp <- extract(r.aspect, current.point)
-    next.point <- current.point + step * c(-sin(aspect.cp), -cos(aspect.cp))
+    # step to the next point and add it to the line
+    next.point <- current.point + step * aspect2vector(aspect.cp)
+    run.away.line <- rbind(run.away.line, next.point)
+    # recalculate the distance to the starting point
     dist2start <- sqrt(sum((start.point - current.point)**2))
-    uphill.line <- rbind(uphill.line, next.point)
+    # preparing the next step
     current.point <- next.point
     n.iter <- n.iter+1
   }
-  return(uphill.line)
+  return(Line(run.away.line))
 }
 
 rsa <- function(r.elev, r.slope, r.aspect, r.pcurv, track.point) {
@@ -32,53 +65,76 @@ rsa <- function(r.elev, r.slope, r.aspect, r.pcurv, track.point) {
   
   # use convex hull function "chull" to make an area out of the points
   
-  max.dist.up <- 500
+  # this should be in a config file
+  max.dist.up <- 200
   max.dist.down <- 50
   max.dist.side <- 50
-  step <- 25
+  step <- 12.5
   
-  # steepest way up from the track point 
-  uphill.line.from.track.point <- run.up(track.point, r.aspect, step, max.dist.up)
+  # Line object with steepest way and down up from the track point 
+  uphill.line.from.track.point <- run.away(track.point, r.aspect, step, max.dist.up, "up")
+  downhill.line.from.track.point <- run.away(track.point, r.aspect, step, max.dist.down, "down")
   
   # make a step left and right
-  aspect.tp <- extract(r.aspect, track.point)
-  point.left <- track.point + max.dist.side * c(-cos(aspect.tp), sin(aspect.tp))
-  point.right <- track.point + max.dist.side * c(cos(aspect.tp), -sin(aspect.tp))
+  left.line.from.track.point <- run.away(track.point, r.aspect, step, max.dist.side, "left")
+  npl <- NROW(left.line.from.track.point@coords)
+  point.left <- matrix(left.line.from.track.point@coords[npl,], ncol = 2)
+  right.line.from.track.point <- run.away(track.point, r.aspect, step, max.dist.side, "right")
+  npr <- NROW(right.line.from.track.point@coords)
+  point.right <- matrix(right.line.from.track.point@coords[npr,], ncol = 2)
   
   # steepest way up from the right and left point 
-  uphill.line.from.left <- run.up(point.left, r.aspect, step, max.dist.up)
-  uphill.line.from.right <- run.up(point.right, r.aspect, step, max.dist.up)
+  uphill.line.from.left <- run.away(point.left, r.aspect, step, max.dist.up, "up")
+  uphill.line.from.right <- run.away(point.right, r.aspect, step, max.dist.up, "up")
+
+  # steepest way down from the right and left points 
+  downhill.line.from.left <- run.away(point.left, r.aspect, step, max.dist.down, "down")
+  downhill.line.from.right <- run.away(point.right, r.aspect, step, max.dist.down, "down")
   
-  all.lines <- SpatialLines(list(Lines(Line(uphill.line.from.track.point), ID="centre"),
-                                 Lines(Line(uphill.line.from.left), ID="left"),
-                                 Lines(Line(uphill.line.from.right), ID="right")))
-  
+  # put everything in a spatialLines object
+  borderLines <- SpatialLines(list(Lines(uphill.line.from.track.point, ID="centre_up"),
+                                 Lines(downhill.line.from.track.point, ID="centre_down"),
+                                 Lines(uphill.line.from.left, ID="left_up"),
+                                 Lines(downhill.line.from.left, ID="left_down"),
+                                 Lines(uphill.line.from.right, ID="right_up"),
+                                 Lines(downhill.line.from.right, ID="right_down")),
+                              proj4string = CRS(proj4string(r.slope)))
+
+  # Construct a polygon of the relevant slope
   # convert the lines into a polygon
-  rsa.points.matrix <- matrix(ncol = 2, nrow = 0)  
-  for (j in (1:3)) {
+  rsa.points.matrix <- matrix(ncol = 2, nrow = 0)
+  for (j in (1:length(borderLines@lines))) {
     rsa.points.matrix <- rbind(rsa.points.matrix,
-                               all.lines@lines[[j]]@Lines[[1]]@coords)
+                               borderLines@lines[[j]]@Lines[[1]]@coords)
   }
-  # convex hull, maybe not ideal
+  # convex hull, maybe not ideal. 'chull' returns the row numbers of the points
+  # forming the hull
   rsa.hull <- rsa.points.matrix[chull(rsa.points.matrix),]
-  # points(rsa.hull, col="green", pch=19)
   # convert the hull to a polygon
-  rsa.polygon <- SpatialPolygons(list(Polygons(list(Polygon(rsa.hull)), 1)))
-  # plot(rsa.polygon, add=T)
-  polygon.raster <- crop(dem.track, extent(rsa.polygon))
-  # crop the slope raster to the polygon
-  r.slope.polygon <- crop(r.slope, extent(rsa.polygon))
-  # select raster points in the polygon
-  points.in.polygon <- over(SpatialPoints(rasterToPoints(r.slope.polygon)), 
-                            rsa.polygon)
-  points.in.polygon[is.na(points.in.polygon)] <- FALSE
-  points.in.polygon[points.in.polygon == 1] <- TRUE
-  sum(values(r.slope.polygon) * points.in.polygon) / sum(points.in.polygon)
-  max(values(r.slope.polygon) * points.in.polygon)
-  plot(r.slope.polygon)
   
-  extract(ras_temp.new, polygon, fun=mean, na.rm=TRUE, sp = T)
+  relevantSlopePolygon <- SpatialPolygons(list(Polygons(list(Polygon(rsa.hull)), ID=1)))
   
-  return(all.lines)
+  # # select raster points in the polygon
+  local.slope <- crop(r.slope, extent(gBuffer(relevantSlopePolygon, width = 50)))
+  slope.points <- rasterToPoints(local.slope) # gives a matrix with x, y, and slope
+  # convert slope points into SpatialPointsDataFrame
+  slope.at.raster.points <- SpatialPointsDataFrame(coords = slope.points[,c('x', 'y')], 
+                                                   data = data.frame(slope = slope.points[,'slope']))
+  # determine the points inside the polygon of the relevant slope
+  slope.points.in.rsa <- intersect(slope.at.raster.points, relevantSlopePolygon)
+  max.slope.in.rsa <- max(slope.points.in.rsa@data$slope)
+  max.slope.point.in.rsa <- slope.points.in.rsa[which.max(slope.points.in.rsa@data$slope),]
+  mean.slope.in.rsa <- mean(slope.points.in.rsa@data$slope)
+  # inverse distance weighted slope angle
+  dist2track.point <- sqrt(rowSums((slope.points.in.rsa@coords - 
+                         matrix(1, nrow = NROW(slope.points.in.rsa@coords)) %*% track.point)^2))
+  dist2track.point[dist2track.point < 6] <- 6
+  sum(slope.points.in.rsa@data$slope * (1/dist2track.point / sum(1/dist2track.point)))
+  #extract(ras_temp.new, polygon, fun=mean, na.rm=TRUE, sp = T)
+  
+  return(slopeProps(borderLines = borderLines,
+                    relevantSlopePolygon = relevantSlopePolygon,
+                    maxSlope = max.slope.point.in.rsa,
+                    meanSlope = mean.slope.in.rsa))
 }
 
--- relevant_slope_test.R
+++ relevant_slope_test.R
@@ -2,70 +2,102 @@
 # test relevant_slope.R
 # -----------------------
 
+# clean up
+rm(list = ls())
+
 wd <- "C:/Documenten/QRM/Rscripts"
-wd <- "D:/QRM/Rscripts"
+# wd <- "D:/QRM/Rscripts"
 setwd(wd)
 
 library(raster)
 library(rgdal)
 library(rgeos)
 source(file = "relevant_slope.R")
-source("DEMderiv.R")
+source(file = "DEMderiv.R")
 
-# read DEM of the Alps
+# read DEM of the Alps in LAEA epsg:3035
 dem.alps <- raster("../DEM/Alps/EUD_CP-DEMS_4500025000-AA.tif")
-# crop Simplon area
+
+# Extent of the Simplon area in WGS84, epsg:4326
 lon.min <- 7.967
 lon.max <- 8.127
 lat.min <- 46.177
 lat.max <- 46.291
 corner.points <- SpatialPoints(coords = matrix(c(lon.min, lon.max, lat.min, lat.max), nrow = 2),
                                proj4string = CRS("+init=epsg:4326"))
-# transform from WGS84 to LAEA
+# transform Simplon extent from WGS84 to LAEA
 extent.simplon <- extent(spTransform(corner.points, CRS("+init=epsg:3035")))
-#elevation
+
+# crop the DEM to the Simplon region
 dem.simplon <- crop(dem.alps, extent.simplon)
+rm(dem.alps) # save some memory
+
+# -- calculate slope attributes --
+# aspect in radians
 aspect.simplon.rad <- terrain(dem.simplon, opt = "aspect", unit = 'radians')
-slope.simplon <- terrain(dem.simplon, opt = "slope", unit = 'degrees')
-plot(slope.simplon>45)
+# slope angle in degrees
+slope.simplon.deg <- terrain(dem.simplon, opt = "slope", unit = 'degrees')
+brks <- c(seq(from = 0, to = 30, by =5), 35, 40, 45, 55, 90)
+nbrks <- length(brks)-1
+slopecols <- c(colorRampPalette(c('white', 'olivedrab1'))(6), 
+               'yellow', 'orange', 'firebrick1', 'darkorchid', 'black')
+png('Slopes_Simlon_deg.png', height = 4*480, width = 4*480,
+     pointsize = 2*12)
+plot(slope.simplon.deg, breaks=brks, col=slopecols, asp=1)
+dev.off()
+# cliffs
+plot(slope.simplon.deg > 45)
 plot(dem.simplon, col = terrain.colors(20))
 
 # plan curvature of the domain
 pcurv.simplon <- DEMderiv(dem.simplon, attr = "plan.curvature", method = "evans")
-plot(pcurv.simplon)
-
+# plot(pcurv.simplon)
 
 # read a track
 gpx.raw <- readOGR(dsn = paste0("Spitzhorli.gpx"), layer = "track_points")
-gpx.3035 <- spTransform(gpx.raw, CRS("+init=epsg:3035"))
-jpeg("spitzhorli.jpg", width = 2*480, height = 2*480)
-track.extent <- extent(gpx.3035)
-track.extent@xmin <- track.extent@xmin-1000
-track.extent@ymin <- track.extent@ymin-1000
-track.extent@xmax <- track.extent@xmax+1000
-track.extent@ymax <- track.extent@ymax+1000
+gpx.track <- spTransform(gpx.raw, proj4string(dem.simplon))
+track.extent <- extent(gBuffer(gpx.track, width = 1000))
 dem.track <- crop(dem.simplon, track.extent)
+
+jpeg("spitzhorli.jpg", width = 2*480, height = 2*480)
 plot(dem.track, col = terrain.colors(20))
-track.line <- SpatialLines(list(Lines(Line(gpx.3035@coords), ID = 1)))
+track.line <- SpatialLines(list(Lines(Line(gpx.track@coords), ID = 1)))
+plot(track.line, add=T)
 
 dev.off()
 i<-21
 source(file = "relevant_slope.R")
-for( i in 1:NROW(gpx.3035@coords)) {
+for( i in 1:NROW(gpx.track@coords)) {
   print(i)
   # convert the track point to a 1x2 matrix
-  track.point <- matrix(gpx.3035@coords[i,], ncol = 2)
+  track.point <- matrix(gpx.track@coords[i,], ncol = 2)
   # determine the relevant slope area
-  rsa.points <- rsa(dem.simplon, slope.simplon, aspect.simplon.rad, pcurv.simplon, track.point)
-  
+  source(file = "relevant_slope.R")
+  rsa.props <- rsa(dem.simplon, slope.simplon.deg, aspect.simplon.rad, pcurv.simplon, track.point)
+
   # plot the DEM, the track, the selected point, it's RSA
-  jpeg(paste0("spitzhorli_", sprintf("_%03d", i), ".jpg"), width = 2*480, height = 2*480)
-  plot(dem.track, col = terrain.colors(20))
+  jpeg(paste0("Spitzhorli/Spitzhorli_", sprintf("_%03d", i), ".jpg"), 
+       width = 4*480, height = 2*480,
+       pointsize = 24)
+  par(mfrow=c(1,2))
+  plot(dem.track, col = terrain.colors(20), asp=1)
   points(track.point, col = 'red', pch=19)
   lines(track.line)
-  lines(rsa.points, col="blue")
+  lines(rsa.props@borderLines, col="blue")
+  
+  # detailed slope plot
+  local.extent <- extent(gBuffer(rsa.props@relevantSlopePolygon, width = 100))
+  local.slope.deg <- crop(slope.simplon.deg, local.extent)
+  plot(local.slope.deg, breaks=brks, col=slopecols, asp=1 )
+  points(track.point, col = 'red', pch=19)
+  lines(track.line)
+  lines(rsa.props@borderLines, col="blue")
+  plot(rsa.props@relevantSlopePolygon, add=T)
+  points(rsa.props@maxSlope)
+  text(x = rsa.props@maxSlope@coords[1], y = rsa.props@maxSlope@coords[2],
+       paste0("Max slope ", round(rsa.props@maxSlope@data$slope)), pos=4)
   dev.off()
-  #chull(as.points(rsa.points))
+  
 }
 
 rsa.points.matrix <- matrix(ncol = 2, nrow = 0)  
@@ -82,7 +114,14 @@ rsa.slope.quant.80 <- quantile(rsa.slopes[[1]], 0.8)
 rsa.slope.mean <- mean(rsa.slopes[[1]])
 rsa.slope.max <- max(rsa.slopes[[1]])
 
+rsa.pcurv <- extract(pcurv.simplon, rsa.polygon)
+rsa.pcurv.mean <- mean(rsa.pcurv[[1]])
 
+extract(slope.simplon, rsa.polygon, cellnumbers=T)
+
+rasterToPoints(slope.simplon)[75656,]
+values(slope.simplon)[75656]
+extract(slope.simplon, matrix(c(4166663, 2573388), ncol = 2)) coordinates(slope.simplon)[75656,])
 
 plot(rsa.polygon, add=T)
 polygon.raster <- crop(dem.track, extent(rsa.polygon))
